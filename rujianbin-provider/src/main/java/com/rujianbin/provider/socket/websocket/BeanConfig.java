34520a4177586e8eb7c9c86994a5022cff8be233
rujianbin <rujianbin@xinyunlian.com>
2017-04-18 15:20:13 +0800

    1

--- rujianbin-provider/src/main/java/com/rujianbin/provider/socket/websocket/BeanConfig.java
+++ rujianbin-provider/src/main/java/com/rujianbin/provider/socket/websocket/BeanConfig.java
@@ -2,6 +2,7 @@ package com.rujianbin.provider.socket.websocket;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.server.standard.ServerEndpointExporter;
 
 /**
  * Created by 汝建斌 on 2017/4/18.
